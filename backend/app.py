70a590159a6db4b4f1c6ec6d0d1bdea627eba72e
CantGoOn <127445646+CantGoOn@users.noreply.github.com>
2024-12-17 19:44:12 -0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -20,4 +20,6 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=7000)
+    #app.run(debug=True, port=7000)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT variable
+    app.run(host="0.0.0.0", port=port, debug=False)
